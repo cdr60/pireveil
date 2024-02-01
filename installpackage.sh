a632e38bfa189f578e828f7762b5bf388371c520
cdr60 <104300119+cdr60@users.noreply.github.com>
2024-02-01 19:06:27 +0100

    Update installpackage.sh

--- installpackage.sh
+++ installpackage.sh
@@ -1,2 +1,2 @@
-apt-get install python3-pip python3-dev python3-rpi.gpio sysstat python3-gpiozero libatlas3-base python3-pil python3-spidev python3-numpy python3-psutil python3-pil -y
+apt-get install python3-pip python3-dev python3-rpi.gpio sysstat python3-gpiozero libatlas3-base python3-pil python3-spidev python3-numpy python3-psutil -y
 
